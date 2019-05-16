7e12bd219feab3a61f026d823ef37afc7103f5dd
David D. Li <lidavid@amazon.com>
2019-05-16 07:50:57 -0700

    [ProductDiscovery] Accept file with list of terms separate by newline

--- src/main/java/com/viral/automation/main/ProductDiscoveryMain.groovy
+++ src/main/java/com/viral/automation/main/ProductDiscoveryMain.groovy
@@ -13,22 +13,40 @@ class ProductDiscoveryMain {
         final String PASSWORD = args[1]
 
         final String TERM = args[2]
-        final String PRESETNAME = args[3]
-        final String PAGESTOTRANSCRIBE = args[4]
+        final String PRESET_NAME = args[3]
+        final String PAGES_TO_TRANSCRIBE = args[4]
 
-        print executeProductDiscovery(EMAIL, PASSWORD, TERM, PRESETNAME, PAGESTOTRANSCRIBE)
+        print executeProductDiscovery(EMAIL, PASSWORD, TERM, PRESET_NAME, PAGES_TO_TRANSCRIBE)
     }
 
     // This is a method serving a different file (the endpoint for bash). In bash we return the result to be written to a file.
-    static String executeProductDiscovery(String email, String password, String term, String presetName, String pagesToTranscribe) {
-        List<String> productDiscoveryResults = new ArrayList<>()
+    static String executeProductDiscovery(
+            final String email,
+            final String password,
+            final String termOrFilename,
+            final String presetName,
+            final String pagesToTranscribe
+    ) {
+        final Set<String> terms = resolveInput(termOrFilename)
 
-        List<Browser> browsers = new ArrayList<>()
+        final List<String> productDiscoveryResults = new ArrayList<>()
+
+        final List<Browser> browsers = new ArrayList<>()
         browsers.add ViralLogin.launch(email, password)
-        browsers.add ViralProductDiscovery.discoverAndRecord(term, presetName, pagesToTranscribe.toInteger(), productDiscoveryResults)
+        for (String term : terms) {
+            browsers.add ViralProductDiscovery.discoverAndRecord(term, presetName, pagesToTranscribe.toInteger(), productDiscoveryResults)
+        }
         browsers.each { it.quit() }
-
         return productDiscoveryResults
     }
-}
 
+    static Set<String> resolveInput(final String termOrFilename) {
+        try {
+            final File fileContainingTerms = new File(termOrFilename)
+            final String rawTerms = fileContainingTerms.getText('UTF-8')
+            return rawTerms.split('\n') as Set
+        } catch (FileNotFoundException e) {
+            return Arrays.asList(termOrFilename)
+        }
+    }
+}
